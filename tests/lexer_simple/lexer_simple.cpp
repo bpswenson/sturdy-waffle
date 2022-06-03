64c48424706e891ad61e02e280b3596bd27ecab8
tommy t freebird <bpswenson@gmail.com>
2022-06-02 23:02:54 -0400

    the testing goat has broken my lexer

--- tests/lexer_simple/lexer_simple.cpp
+++ tests/lexer_simple/lexer_simple.cpp
@@ -10,11 +10,11 @@ using namespace xeon;
 
 TEST_CASE("Lexer: Simple Int", "[Lexer]") {
 
-    std::string test_int = "53";
+    std::string test_input = "53";
 
     sptr<SourceManager> source_manager = std::make_shared<SourceManager>();
 
-    source_manager->AddNewSourceBuffer(MemoryBuffer::getMemBuffer(test_int, "testIntBuffer"), SourceLocation());
+    source_manager->AddNewSourceBuffer(MemoryBuffer::getMemBuffer(test_input), SourceLocation());
 
     Lexer lex = Lexer(source_manager);
 
@@ -27,11 +27,11 @@ TEST_CASE("Lexer: Simple Int", "[Lexer]") {
 
 TEST_CASE("Lexer: Simple Float", "[Lexer]") {
 
-    std::string test_int = "53.11";
+    std::string test_input = "53.11";
 
     sptr<SourceManager> source_manager = std::make_shared<SourceManager>();
 
-    source_manager->AddNewSourceBuffer(MemoryBuffer::getMemBuffer(test_int, "testFloatBuffer"), SourceLocation());
+    source_manager->AddNewSourceBuffer(MemoryBuffer::getMemBuffer(test_input), SourceLocation());
 
     Lexer lex = Lexer(source_manager);
 
@@ -43,11 +43,11 @@ TEST_CASE("Lexer: Simple Float", "[Lexer]") {
 
 TEST_CASE("Lexer: Simple String", "[Lexer]") {
 
-    std::string test_int = "\"FooManChu\"";
+    std::string test_input = "\"FooManChu\"";
 
     sptr<SourceManager> source_manager = std::make_shared<SourceManager>();
 
-    source_manager->AddNewSourceBuffer(MemoryBuffer::getMemBuffer(test_int, "testFloatBuffer"), SourceLocation());
+    source_manager->AddNewSourceBuffer(MemoryBuffer::getMemBuffer(test_input), SourceLocation());
 
     Lexer lex = Lexer(source_manager);
 
@@ -55,4 +55,36 @@ TEST_CASE("Lexer: Simple String", "[Lexer]") {
 
     REQUIRE(tok.get_type() == TokenType::string_literal);
     REQUIRE(tok.get_context() == "\"FooManChu\"");
+}
+
+TEST_CASE("Lexer: IntStringFloat", "[Lexer]") {
+    std::string test_input = R"(23 523.33 "Eat at Joes" "Bar and Grill")";
+
+    sptr<SourceManager> source_manager = std::make_shared<SourceManager>();
+
+    source_manager->AddNewSourceBuffer(MemoryBuffer::getMemBuffer(test_input), SourceLocation());
+
+    Lexer lex = Lexer(source_manager);
+
+    Token tok = lex.get_next_token();
+    REQUIRE(tok.get_type() == TokenType::integer_literal);
+    REQUIRE(tok.get_context() == "23");
+
+    tok = lex.get_next_token();
+
+    REQUIRE(tok.get_type() == TokenType::float_literal);
+    REQUIRE(tok.get_context() == "523.33");
+
+    tok = lex.get_next_token();
+
+    REQUIRE(tok.get_type() == TokenType::string_literal);
+    REQUIRE(tok.get_context() == "\"Eat at Joes\"");
+
+    tok = lex.get_next_token();
+
+    REQUIRE(tok.get_type() == TokenType::string_literal);
+    REQUIRE(tok.get_context() == "\"Bar and Grill\"");
+
+
+
 }
